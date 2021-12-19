f66ba2d62be0372be88e083f93647f6305fd8832
Kevin Ha <kevinha298@live.com>
2021-12-19 10:46:56 -0800

    Added proxy.

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from django.conf.urls import url 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.core import MemberList, MemberDetail
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +30,9 @@ urlpatterns = [
     path('core/token/refresh/', TokenRefreshView.as_view()),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
 
